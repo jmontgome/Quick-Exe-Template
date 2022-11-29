e523e374f4c020e262116db23731402b9d7a4bdc
jmontgome <87336298+jmontgome@users.noreply.github.com>
2022-11-28 19:28:10 -0600

    Add project files.

---
+++ QuickExeTemplate/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace QuickExeTemplate
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
